e52d61316c1baac655a026cacf407df27db1d068
Erik Lalancette <linux1@gmail.com>
2023-12-18 15:16:45 -0500

    Update client.py

--- client.py
+++ client.py
@@ -8,12 +8,6 @@ def main():
     host = 'localhost'
     port = 8080
 
-    with open('tls/tls.crt', 'rb') as f:
-        trusted_certs = f.read()
-
-    credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
-    channel = grpc.secure_channel('{}:{}'.format(host, port), credentials)
-
     stub = service_pb2_grpc.ServerStub(channel)
     stub.Foo(service_pb2.Empty())
 
